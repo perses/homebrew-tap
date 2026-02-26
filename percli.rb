42aaa323b061264ad8782ba444ee9487237dda47
Augustin Husson <augustin.husson@amadeus.com>
2026-02-26 13:16:38 +0100

    Release v0.53.0
    Signed-off-by: Augustin Husson <augustin.husson@amadeus.com>

--- percli.rb
+++ percli.rb
@@ -1,25 +1,24 @@
 class Percli < Formula
   desc 'Command line client for the CNCF sandbox for observability visualisation'
   homepage 'https://perses.dev'
-  version '0.52.0'
-  sha256 '0a584c627b7c76b36883e77506359ccde52ed5e04592be4b8415ae85cd136061'
+  version '0.53.0'
   license 'Apache-2.0'
 
   if OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/perses/perses/releases/download/v#{version}/perses_#{version}_linux_arm64.tar.gz"
-      sha256 '4a9c0fb2abc6338a21a7753747566a3a7f505a822d6517e054ebd2245afb380e'
+      sha256 '796b7f0f0f4bd80f830d382146b18a416024dd3d6113757dcefe378879d58bbe'
     else
       url "https://github.com/perses/perses/releases/download/v#{version}/perses_#{version}_linux_amd64.tar.gz"
-      sha256 'cbfd4c12069762b9e1d66d5658339cac09ae0856f5d1683603e59a90f605078e'
+      sha256 '4c1a07c05c306cf3b009a8ef62b092aaac9b444d02d6e7366d7487f6f6899bb7'
     end
   else
     if Hardware::CPU.arm?
       url "https://github.com/perses/perses/releases/download/v#{version}/perses_#{version}_darwin_arm64.tar.gz"
-      sha256 '206faf009bd419955d9bc633226cd0f24807408d45eae10c40a148c78ee17f26'
+      sha256 '9323792108434ed3d9a87edf4a2cc409923aa330a5f16d78b376e092ccdc4098'
     else
       url "https://github.com/perses/perses/releases/download/v#{version}/perses_#{version}_darwin_amd64.tar.gz"
-      sha256 'afb68bc8d99c6d31ae7367f5b452c7ef9334cb73ea919335a3243eaac4c3804b'
+      sha256 '81b710a11a4db60173aa45eb1d0f40d661a055bb83d0ac1359da67b9cf5695ea'
     end
   end
 
